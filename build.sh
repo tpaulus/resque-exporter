07c35bbf44e6b75c91fb1af33c1d565a6fa7edb5
Glauber Batista <glauber@jobscore.com>
2022-05-23 15:38:24 -0300

    Add build script to build the exporter in different plataforms since the promu crossbuild doesn't work properly

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+PREFIX=$(pwd)/.build/
+platforms=("386" "amd64" "arm64"  "mips" "mips64" "mips64le" "mipsle" "ppc64" "ppc64le" "s390x")
+[ -d "${PREFIX}" ] && rm -rf ${PREFIX}
+[ ! -d "${PREFIX}" ] && mkdir -p ${PREFIX}
+
+for platform in ${platforms[*]}; do
+  GOOS=linux GOARCH=${platform} PREFIX=${PREFIX} make build
+  filename=resque-exporter-$(cat VERSION).${platform}.tar.gz
+  tar -cvzf ${PREFIX}${filename} -C ${PREFIX} resque-exporter
+  # option --remove-files is not supported on macos, so we remove the file manually
+  rm ${PREFIX}resque-exporter
+  echo "`openssl sha256 ${PREFIX}${filename} | awk '{ print $2 }'` ${filename}" >> ${PREFIX}sha256sums.txt
+done
